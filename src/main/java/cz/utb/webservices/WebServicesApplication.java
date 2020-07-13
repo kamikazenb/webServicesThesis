a9396f469ceac4e3eaa2ec0afa2f0c30965692fe
KamikazeNB <kamikazenb@gmail.com>
2020-07-13 13:25:00 +0200

    init commit

---
+++ src/main/java/cz/utb/webservices/WebServicesApplication.java
@@ -0,0 +1,13 @@
+package cz.utb.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/utb/webservices/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.utb.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

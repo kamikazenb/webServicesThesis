e6ebadff61bcf4be75f0969c25377d8f593dad67
KamikazeNB <kamikazenb@gmail.com>
2020-07-15 21:36:48 +0200

    added basic SSE

--- src/main/java/cz/utb/webservices/api/TouchController.java
+++ src/main/java/cz/utb/webservices/api/TouchController.java
@@ -1,14 +1,31 @@
 package cz.utb.webservices.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import cz.utb.webservices.model.Touch;
 import cz.utb.webservices.service.TouchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 import javax.validation.Valid;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.sse.Sse;
+import javax.ws.rs.sse.SseEventSink;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
+import java.util.stream.Stream;
+
 
 @RequestMapping("api/v1/touch")
 @RestController
@@ -31,18 +48,21 @@ public class TouchController {
         return touchService.getAllTouches();
     }
 
-    /*@GetMapping(path = "{id}")
-    public Touch getTouchById(@PathVariable("id") UUID id) {
-        return touchService.getTouchById(id).orElse(null);
+    @GetMapping("/stream-sse")
+    public Flux<ServerSentEvent<String>> streamEvents() {
+        String json = "";
+        try {
+            json = new ObjectMapper().
+                    writeValueAsString(new Touch(0, 0, "TouchUp", "Created", "recived", 5));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        final String result = json;
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> ServerSentEvent.<String>builder()
+                        .id(String.valueOf(sequence))
+                        .event("periodic-event")
+                        .data(result)
+                        .build());
     }
-
-    @DeleteMapping(path = "{id}")
-    public void deletePersonById(@PathVariable("id") UUID id) {
-        touchService.deleteTouch(id);
-    }
-
-    @PutMapping(path = "{id}")
-    public void updateTouch(@PathVariable("id") UUID id, @Valid @NonNull @RequestBody Touch touchUpdate) {
-        touchService.updateTouch(id, touchUpdate);
-    }*/
 }
